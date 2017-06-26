d987b0fe6c8e4b1b1ade13cd7f741abfd104be4f
Sergio Alexander <xergioalex@gmail.com>
2017-06-25 20:10:02 -0500

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+// Application name	platzimusic
+// API key	243b7d6b032fdd8076066339c000b211
+// Shared secret	f4a05d27b59e0fdbbd4b866db33adfc5
+// Registered to	xergioalex
+
+export default {
+	appname: 'platzimusic',
+	apiKey: '243b7d6b032fdd8076066339c000b211',
+	secret: 'f4a05d27b59e0fdbbd4b866db33adfc5',
+	registeredTo: 'xergioalex'
+}
